487d50b8102352d20aa57efc8598307feb081f4a
gianbarbaro <gianlbarbaro@gmail.com>
2022-04-25 20:50:49 -0300

    cambios entidad curso y emun

--- src/main/java/com/proyectoFinal/entidades/Curso.java
+++ src/main/java/com/proyectoFinal/entidades/Curso.java
@@ -1,7 +1,90 @@
 
 package com.proyectoFinal.entidades;
 
+import com.proyectoFinal.enums.Nivel;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
-public class Curso {
+@Entity
+@Table(name = "Cursos")
+public class Curso implements Serializable {
     
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    @Column(name = "nombre_del_curso")
+    private String nombre;
+    @Column(name = "nivel_del_curso")
+    private Nivel nivel;
+    @OneToMany
+    private List<Usuario> alumnos;
+    @OneToOne
+    private Usuario profesor;
+
+    public Curso(String id, String nombre, Nivel nivel, List<Usuario> alumnos, Usuario profesor) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.alumnos = alumnos;
+        this.profesor = profesor;
+    }
+
+    public Curso() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Nivel getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Nivel nivel) {
+        this.nivel = nivel;
+    }
+
+    public List<Usuario> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(List<Usuario> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public Usuario getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Usuario profesor) {
+        this.profesor = profesor;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "id=" + id + ", nombre=" + nombre + ", nivel=" + nivel + ", alumnos=" + alumnos + ", profesor=" + profesor + '}';
+    }
 }
---
+++ src/main/java/com/proyectoFinal/enums/Nivel.java
@@ -0,0 +1,6 @@
+
+package com.proyectoFinal.enums;
+
+public enum Nivel {
+    A1, A2, B1, B2, C1, C2
+}
