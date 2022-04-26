93f2c8b86a3dafbb641711e2411f10cce609fb7e
Leonardo <leonardolillo9@gmail.com>
2022-04-25 21:31:22 -0300

    Agregue servicios, repositorios y controladores

---
+++ src/main/java/com/proyectoFinal/controladores/UsuarioControlador.java
@@ -0,0 +1,63 @@
+package com.proyectoFinal.controladores;
+
+import com.proyectoFinal.entidades.Usuario;
+import com.proyectoFinal.servicios.UsuarioServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioControlador {
+
+    @Autowired
+    UsuarioServicio usuarioServicio;
+
+    @GetMapping("/list-usuario")
+    public String listarUsuarios(ModelMap model) {
+
+        List<Usuario> usuarios = usuarioServicio.listarUsuarios();
+
+        model.addAttribute("usuarios", usuarios);
+
+        return "list-usuario";
+    }
+
+    @GetMapping("/form-usuario")
+    public String formulario() {
+        return "form-usuario";
+    }
+
+    @PostMapping("/form-usuario")
+    public String guardar(ModelMap model, @RequestParam String nombre, @RequestParam String apellido, @RequestParam Integer dni, @RequestParam String email, @RequestParam Integer telefono, @RequestParam String password) {
+
+        try {
+            usuarioServicio.crear(nombre, apellido, dni, email, telefono, password);
+
+            return "redirect:/usuario/index";
+        } catch (Exception e) {
+
+            return "list-usuario";
+        }
+    }
+
+    @GetMapping("/eliminar-usuario/{id}")
+    public String eliminar(@PathVariable String id) {
+
+        usuarioServicio.eliminarUsuario(id);
+
+        return "redirect:/usuario/list-usuario/";
+
+    }
+
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/proyectoFinal/servicios/UsuarioServicio.java
@@ -0,0 +1,123 @@
+package com.proyectoFinal.servicios;
+
+import com.proyectoFinal.entidades.Usuario;
+import com.proyectoFinal.enums.Rol;
+import com.proyectoFinal.repositorios.UsuarioRepositorio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Service
+public class UsuarioServicio implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepositorio usuarioRepositorio;
+
+    @Transactional(rollbackFor = {Exception.class})
+    public Usuario crear(String nombre, String apellido, Integer dni, String email, Integer telefono, String password) throws Exception {
+
+        validarUsuario(nombre, apellido, dni);
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre(nombre);
+        usuario.setApellido(apellido);
+        usuario.setDni(dni);
+        usuario.setEmail(email);
+        usuario.setTelefono(telefono);
+        String claveEncriptada = new BCryptPasswordEncoder().encode(password);
+        usuario.setPassword(claveEncriptada);
+        usuario.setRol(Rol.ALUMNO);
+
+        return usuarioRepositorio.save(usuario);
+
+    }
+
+    @Transactional(rollbackFor = {Exception.class})
+    public void modificar(String id, String nombre, String apellido, Integer dni, String email, Integer telefono, String password) throws Exception {
+
+        validarUsuario(nombre, apellido, dni);
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(id);
+
+        if (respuesta.isPresent()) {
+
+            Usuario usuario = respuesta.get();
+            usuario.setNombre(nombre);
+            usuario.setApellido(apellido);
+            usuario.setDni(dni);
+            usuario.setEmail(email);
+            usuario.setTelefono(telefono);
+            String claveEncriptada = new BCryptPasswordEncoder().encode(password);
+            usuario.setPassword(claveEncriptada);
+            usuario.setRol(Rol.ALUMNO);
+            usuarioRepositorio.save(usuario);
+
+        } else {
+            throw new Exception("No se encontro el usuario buscado");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> listarUsuarios() {
+
+        return usuarioRepositorio.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Usuario> listarId(String id) {
+        return usuarioRepositorio.findById(id);
+    }
+
+    public void eliminarUsuario(String id) {
+
+        usuarioRepositorio.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario u = usuarioRepositorio.buscarPorEmail(email);
+
+        if (u == null) {
+            return null;
+        }
+
+        List<GrantedAuthority> permisos = new ArrayList<>();
+
+        GrantedAuthority p1 = new SimpleGrantedAuthority("ROLE_" + u.getRol().toString());
+        permisos.add(p1);
+
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+        HttpSession session = attr.getRequest().getSession(true);
+        session.setAttribute("usuariosession", u);
+
+        return new User(u.getEmail(), u.getPassword(), permisos);
+    }
+
+    private void validarUsuario(String nombre, String apellido, Integer dni) throws Exception {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new Exception("El nombre no puede ser nulo");
+        }
+
+        if (apellido == null || apellido.isEmpty()) {
+            throw new Exception("El apellido no puede ser nulo");
+        }
+        if (dni == null) {
+            throw new Exception("El dni no puede ser nulo");
+        }
+
+    }
+}
