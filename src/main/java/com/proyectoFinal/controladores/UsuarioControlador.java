2233f403109b1901fca0a0643879da5109c55474
Franco Costantini <costantinifranco2001@gmail.com>
2022-04-27 22:07:07 -0300

    cambio ranzio

--- src/main/java/com/proyectoFinal/controladores/UsuarioControlador.java
+++ src/main/java/com/proyectoFinal/controladores/UsuarioControlador.java
@@ -30,15 +30,7 @@ public class UsuarioControlador {
     }
 
     
-      @GetMapping("/list-usuario")
-    public String dsadsa(ModelMap model) {
-
-        List<Usuario> usuarios = usuarioServicio.listarUsuarios();
-
-        model.addAttribute("usuarios", usuarios);
-
-        return "list-usuario";
-    }
+    
     @GetMapping("/form-usuario")
     public String formulario() {
         return "form-usuario";
